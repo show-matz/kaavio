721e340970828c897a204c22bad6494e7252f0b0
Show MATSUOKA <show@architect-matsuoka.jpn.org>
2020-03-25 18:50:19 +0900

    037QO0Om4sK : fill-info に rule を追加
    	modified:   sample/tmp.diagram
    	modified:   sample/tmp.svg
    	modified:   src/fill-info.lisp

--- src/fill-info.lisp
+++ src/fill-info.lisp
@@ -28,7 +28,11 @@
    (opacity	;:type     number
 			:initform nil
 			:initarg  :opacity
-			:accessor fill-opacity)))
+			:accessor fill-opacity)
+   (rule	;:type     (or nil keyword)
+			:initform nil
+			:initarg  :rule
+			:accessor fill-rule)))
 
 
 (defmethod initialize-instance :after ((fill fill-info) &rest initargs)
@@ -40,16 +44,22 @@
   (declare (ignore canvas dict))
   (check-member (color   (fill-color   fill)) :nullable t :types (or string keyword))
   (check-member (opacity (fill-opacity fill)) :nullable t :types number)
+  (check-member (rule    (fill-rule    fill)) :nullable t :types keyword)
+  (when (fill-rule fill)
+	(check-keywords (rule (fill-rule fill)) :nonzero :evenodd))
   t)
 
 (defmethod to-property-strings ((fill fill-info))
-  (let ((color   (fill-color     fill))
-		(opacity (fill-opacity   fill)))
+  (let ((color   (fill-color   fill))
+		(opacity (fill-opacity fill))
+		(rule    (fill-rule    fill)))
 	(when color
 	  (setf color (format-string "fill='" color "' ")))
 	(when opacity
 	  (setf opacity (format-string "fill-opacity='" opacity "' ")))
-	(concatenate 'string color opacity)))
+	(when rule
+	  (setf rule (format-string "fill-rule='" rule "' ")))
+	(concatenate 'string color opacity rule)))
 
 
 #|
@@ -65,18 +75,21 @@
 	  (if (and (null params) *default-fill*)
 		  *default-fill*
 		  (destructuring-bind (&key (color   nil   color-p)
-									(opacity nil opacity-p) base) params
+									(opacity nil opacity-p)
+									(rule    nil    rule-p) base) params
 			(let ((base (or base *default-fill*)))
 			  (labels ((fixval (val-p val base-fnc default)
 						 (if val-p val (if base (funcall base-fnc base) default))))
 				(make-instance 'fill-info
 							   :color   (fixval color-p   color   #'fill-color  :none)
-							   :opacity (fixval opacity-p opacity #'fill-opacity  nil))))))))
+							   :opacity (fixval opacity-p opacity #'fill-opacity  nil)
+							   :rule    (fixval rule-p    rule    #'fill-rule     nil))))))))
 
 
 
 (setf *default-fill* (make-fill :color :none
-								:opacity nil))
+								:opacity nil
+								:rule    nil))
 
 ;;(to-property-strings (make-fill))
 ;;(to-property-strings (make-fill :red))
