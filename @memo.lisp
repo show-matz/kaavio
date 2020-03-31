53b14e65963e34aaa158a1d9132d3255ccac34ba
Show MATSUOKA <show@architect-matsuoka.jpn.org>
2020-03-31 18:22:23 +0900

    ファイル日付を利用して生成対象の sample を制御するように @memo.lisp を改善
    	modified:   @memo.lisp

--- @memo.lisp
+++ @memo.lisp
@@ -12,9 +12,18 @@
 										 "C:/sandbox/cl-diagram/sample/"))
 (diagram::path/get-current-directory)
 
-(dolist (file (path:list-directory (diagram::path/get-current-directory)))
-  (when (string= "diagram" (pathname-type file))
-	(let ((outfile (make-pathname :type "svg" :defaults file)))
-	  (format t "~A~%" file)
-	  (diagram::cl-apps-main `(,file :utf8 ,outfile :utf8)))))
+(defun update-svg-files (&optional force-all-p)
+  (let ((pred (if force-all-p
+				  (constantly t)
+				  (lambda (diagram-file svg-file)
+					(or (not (path:is-existing-file svg-file))
+						(let ((time1 (path:get-time diagram-file))
+							  (time2 (path:get-time     svg-file)))
+						  (string< time2 time1)))))))
+	(dolist (file (path:list-directory (diagram::path/get-current-directory)))
+	  (when (string= "diagram" (pathname-type file))
+		(let ((outfile (make-pathname :type "svg" :defaults file)))
+		  (when (funcall pred file outfile)
+			(format t "~A~%" file)
+			(diagram::cl-apps-main `(,file :utf8 ,outfile :utf8))))))))
 
