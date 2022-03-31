eb73b4ad10a9a35fd2d8e7c55ea43fd0097f1d80
Show MATSUOKA <show@architect-matsuoka.jp>
2022-04-01 00:47:35 +0900

    9AVzEnvPuhJ : 単体起動プログラムに -I オプションを追加
    	modified:   misc/cl-diagram-main.lisp

--- misc/cl-diagram-main.lisp
+++ misc/cl-diagram-main.lisp
@@ -3,6 +3,7 @@
 (in-package :cl-diagram)    ;;ToDo : :diagram-user でもいける？
 
 (defun cl-diagram-main (self args)
+  (declare (ignorable self))
   (handler-bind ((condition
 				  (lambda (c)
 					(let ((is-warn (typep c 'warning)))
@@ -14,25 +15,37 @@
 					  (if is-warn
 						  (muffle-warning)
 						  (return-from cl-diagram-main nil))))))
-
-	;; 起動した実行可能ファイルと同じ場所にある lib ディレクトリ配下を include-path に指定する
-	;; ToDo : 環境によってはフルパスを取れないのでこれではダメっぽい
-	(let* ((dir-list (append (pathname-directory self) (list "lib")))
-		   (lib-path (make-pathname :directory dir-list
-									:name nil :type nil :defaults self)))
-	  (setf diagram:*include-paths* (list lib-path)))
-
-	(if (< 1 (length args))
-		(diagram::throw-exception "Invalid parameter count.")
-		(labels ((read-whole (stream acc)
-				   (let ((lst (read stream nil nil)))
-					 (if (null lst)
-						 (cons 'progn (nreverse acc))
-						 (read-whole stream (cons lst acc)))))
-				 (impl (stream)
-				   (let ((*package* (find-package :diagram-user)))
-					 (write-string (eval (read-whole stream nil))))))
-		  (if (zerop (length args))
-			  (impl *standard-input*)
-			  (with-open-file (stream (car args) :direction :input)
-				(impl stream)))))))
+	
+	;; ToDo : --help  オプションに対応する
+	(labels ((is-I-option (param)
+			   (and (< 2 (length param))
+					(string= "-I" (subseq param 0 2))))
+			 (fix-path (param)
+			   (let ((path (subseq param 2)))
+				 (if (char= #\/ (char path (1- (length path))))
+					 path
+					 (concatenate 'string path "/"))))
+			 (retrieve-include-paths (args &optional paths)
+			   (if (null args)
+				   (values (nreverse paths) nil)
+				   (let ((param (car args)))
+					 (if (not (is-I-option param))
+						 (values (nreverse paths) args)
+						 (retrieve-include-paths (cdr args)
+												 (cons (fix-path param) paths)))))))
+	  (multiple-value-bind (paths args) (retrieve-include-paths args)
+		(let ((diagram:*include-paths* paths))
+		  (if (< 1 (length args))
+			  (diagram::throw-exception "Invalid parameter count.")
+			  (labels ((read-whole (stream acc)
+						 (let ((lst (read stream nil nil)))
+						   (if (null lst)
+							   (cons 'progn (nreverse acc))
+							   (read-whole stream (cons lst acc)))))
+					   (impl (stream)
+						 (let ((*package* (find-package :diagram-user)))
+						   (write-string (eval (read-whole stream nil))))))
+				(if (zerop (length args))
+					(impl *standard-input*)
+					(with-open-file (stream (car args) :direction :input)
+					  (impl stream))))))))))
