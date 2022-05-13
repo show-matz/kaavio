a237326d4c503ca186b3094ce66c1c6f996bd7a5
Show MATSUOKA <show@architect-matsuoka.jp>
2022-05-13 12:26:52 +0900

    5vCGUM6KVSv : dictionary の history 実装を配列からリストに変更

--- src/dictionary.lisp
+++ src/dictionary.lisp
@@ -16,36 +16,31 @@
  |#
 (defclass dictionary ()
   ((history-max		:initform   0 :initarg :hist-max)	; integer
-   (history-arr		:initform nil :initarg :hist-arr)	; array
+   (history-top		:initform   0 :initarg :hist-top)	; list
    (history-size	:initform   0 :initarg :hist-size)	; integer
-   (history-top		:initform   0 :initarg :hist-top)	; integer
    (map				:initform nil :initarg :map)))		; hashtable
 
-(defun dict-create (history-size)
+(defun dict-create (history-max-count)
   (make-instance 'dictionary
-				 :hist-max  history-size
-				 :hist-arr  (make-array history-size :initial-element nil)
+				 :hist-max  history-max-count
+				 :hist-top  nil
 				 :hist-size 0
-				 :hist-top  0
 				 :map       (make-hash-table :test 'eq)))
 				 
 
 (defun dict-register (dict entity)
-  (with-slots (history-max history-arr
-			   history-size history-top map) dict
+  (with-slots (history-max history-top history-size map) dict
 	(let ((id (slot-value entity 'id)))
 	  (when id
-		(when (or (eq id :canvas)
-				  (gethash id map))
+		(when (or (eq id :canvas) (gethash id map))
 		  (throw-exception "ID '~A' has already exist in dictionary." id))
 		(setf (gethash id map) entity)))
-	(decf history-top)
-	(when (< history-top 0)
-	  (setf history-top (1- history-max)))
-	(setf (svref history-arr history-top) entity)
-	(when (< history-size history-max)
-	  (incf history-size))
-	entity))
+	(if (= history-size history-max)
+		(setf history-top (cons entity (nbutlast history-top 1)))
+		(progn
+		  (incf history-size)
+		  (setf history-top (cons entity history-top)))))
+  entity)
 
 (defun dict-get-entity (dict id)
   (labels ((history-kwd-p (id)
@@ -55,15 +50,11 @@
 					 (parse-integer str :start 1 :junk-allowed t)
 				   (when (= end (length str))
 					 idx))))))
-	(let ((index (history-kwd-p id)))
-	  (with-slots (history-max history-arr
-							   history-size history-top map) dict
+	(with-slots (history-max history-top history-size map) dict
+	  (let ((index (history-kwd-p id)))
 		(if (null index)
 			(gethash id map)
 			(if (or (< index 1) (< history-size index))
 				(throw-exception "ID '~A' is out of history range." id)
-				(svref history-arr
-					   (mod (+ history-top (1- index)) history-max))))))))
-
-
+				(nth (1- index) history-top)))))))
 
